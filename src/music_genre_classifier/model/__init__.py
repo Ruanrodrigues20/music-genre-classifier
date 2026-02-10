7d7514aa833af252bbe5838833f57a4338be41cc
Ruanrodrigues20 <ruan.rodrigues.silvaa@gmail.com>
2026-02-10 20:48:00 -0300

    preprocess dataset

---
+++ src/music_genre_classifier/model/__init__.py
@@ -0,0 +1,3 @@
+from music_genre_classifier.model.genre_type import GenreType
+
+__all__ = ["GenreType"]
---
+++ src/music_genre_classifier/model/genre_type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class GenreType(Enum):
+    POP = "pop"
+    ROCK = "rock"
+    ELETRONICA = "eletronica"
+    CLASSICA = "classica"
+    FORRO = "forro"
